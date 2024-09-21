6bcfc5609b790b52e86143cdf07338402a9e8d46
ysysimon <ysysimon@live.com>
2024-09-22 02:19:07 +0800

    add first controller test and add drogon dirctly into buildtree (try to avoid linking error on windows)

---
+++ YLineServer/controllers/TestCtrl.cc
@@ -0,0 +1,11 @@
+#include "TestCtrl.h"
+
+void TestCtrl::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+    auto resp = HttpResponse::newHttpResponse();
+    resp->setStatusCode(drogon::k200OK);
+    resp->setContentTypeCode(drogon::CT_TEXT_PLAIN);
+    resp->setBody("Hello, World!");
+    callback(resp);
+}
--- linux_cmake_config.sh
+++ linux_cmake_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running CMake with Ninja generator..."
-cmake -G Ninja -B target -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -G Ninja -B target -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DUSE_SPDLOG=ON -DBUILD_CTL=OFF
 
 # if std not found on Linux, maybe need to install libstdc++-12-dev
 # use .clangd instead of copy compile_commands.json,
