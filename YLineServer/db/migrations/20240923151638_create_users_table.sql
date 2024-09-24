fe12bcb760e9cfb8f3de7cc6837ac3ce5cbddf6e
Zeno <ysysimon@live.com>
2024-09-24 19:24:25 +0800

    add user table

--- YLineServer/db/migrations/20240923151638_create_users_table.sql
+++ YLineServer/db/migrations/20240923151638_create_users_table.sql
@@ -2,8 +2,9 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
