81d71590d33b9ee5aacb12004e64fc82309aa4fd
Zeno <ysysimon@live.com>
2025-01-13 18:58:08 +0800

    (YLineServer) change nothing, stll designing the system of consuming message

---
+++ YLineServer/inc/components/worker.h
@@ -0,0 +1,19 @@
+#ifndef YLineServer_Componets_WORKER_H
+#define YLineServer_Componets_WORKER_H
+
+#include "utils/server.h"
+
+namespace YLineServer::Components
+{
+
+struct Consumer
+{
+// TODO
+// need to design a good way to consume messages
+};
+
+}; // namespace YLineServer::Components
+
+
+
+#endif // YLineServer_Componets_WORKER_H
